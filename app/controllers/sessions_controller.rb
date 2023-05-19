314f23c035e46d102d525e2382186736083c8488
Ronika Bhatti <ronika.bhatti@kellogg.northwestern.edu>
2023-05-19 04:11:14 +0000

    thru step 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,26 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+    end
   end
+
+def destroy
+  flash["notice"] = "Goodbye."
+  session["user_id"] = nil
+  redirect_to "/login"
+end
+
 end
